6138bff921153e19035cc9b7151581d71c743fa7
Huevo <84205027+Rodrigo92HW@users.noreply.github.com>
2023-04-16 15:58:53 -0300

    Base

---
+++ app.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsHqXAhdRYJCocmHT9TD2JuB_9O-sRjcM",
+    authDomain: "feche-e1193.firebaseapp.com",
+    projectId: "feche-e1193",
+    storageBucket: "feche-e1193.appspot.com",
+    messagingSenderId: "236658476401",
+    appId: "1:236658476401:web:a134e83748e7b68d0be2d0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase();
+const reference = ref(db, 'number');
+
+let i = 0;
+setInterval(increment, 1000);
+
+function increment() {
+    i++;
+    document.getElementById('counter').textContent = i;
+
+    set(reference, i)
+    .catch(error => console.log(error.message));
+}
