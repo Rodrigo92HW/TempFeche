26141b8c59408fb91a6140fd7054f8237c6bb4bc
Huevo <84205027+Rodrigo92HW@users.noreply.github.com>
2023-04-17 03:30:08 -0300

    56

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue, set } from 'firebase/database';
-
-console.log('app.js is running');
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAsHqXAhdRYJCocmHT9TD2JuB_9O-sRjcM",
-    authDomain: "feche-e1193.firebaseapp.com",
-    projectId: "feche-e1193",
-    storageBucket: "feche-e1193.appspot.com",
-    messagingSenderId: "236658476401",
-    appId: "1:236658476401:web:a134e83748e7b68d0be2d0"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase();
-const reference = ref(db, 'number');
+const app = window.firebase.initializeApp(firebaseConfig);
+const db = window.firebase.database();
+const reference = db.ref('number');
 
 // Set the initial value of 'number' in the database to 0
-set(reference, 0)
+reference.set(0)
     .catch(error => console.log(error.message));
 
 let i = 0;
-onValue(reference, (snapshot) => {
+reference.on('value', (snapshot) => {
     // Get the current value of 'number' from the database
     const number = snapshot.val() || 0;
     // Update the counter with the current value of 'number'
     document.getElementById('counter').textContent = number;
     // Increment 'i' and set the new value of 'number' in the database
     i = number + 1;
-    set(reference, i)
+    reference.set(i)
         .catch(error => console.log(error.message));
-    console.log(i);
 });
